3c5fb2e491ccad3cb6f49899c173034ff609a1cd
RixarshXD <166057490+RixarshXD@users.noreply.github.com>
2024-06-06 04:50:49 -0400

    modificaciones

--- assets/js/promesas.js
+++ assets/js/promesas.js
@@ -1,4 +1,4 @@
-import { addDoc, collection } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"
+import { addDoc, collection, getDocs, updateDoc, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"
 import { db } from "./firebase.js"
 
 
@@ -6,4 +6,29 @@ import { db } from "./firebase.js"
 export const registrarGuitarra = async(guitarras)=>{
     console.log(guitarras)
     const docRef = await addDoc(collection(db,'guitarras'),guitarras);
-}
+};
+
+// obtener los registros de la base de datos 
+export const obtenerGuitarras = async ()=>{
+    const ref = collection(db,'guitarras');
+    const querySnapshot = await getDocs(ref);
+
+    let guitarras = []
+    querySnapshot.forEach((doc) => {
+        guitarras.push({...doc.data(), id:doc.id})
+    }); 
+    return guitarras
+};
+      
+// se crea la funcion para actualizar los datos 
+export const actualizarGuitarra = async(guitarras,id) => {
+    const referencia = doc(db, 'guitarras', id);
+    await updateDoc(referencia,guitarras)
+};
+
+
+// eliminar datos registrados
+export const eliminarGuitarra = async(id) => {
+    const referencia = doc(db, 'guitarras', id);
+    await deleteDoc(referencia);
+};
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
-import { registrarGuitarra } from "./promesas.js";
+import { actualizarGuitarra, eliminarGuitarra, obtenerGuitarras, registrarGuitarra } from "./promesas.js";
 
 window.addEventListener('load',()=>{
     document.getElementById('btnColor').addEventListener('click',cambiarColor);
-    document.getElementById('btnEnviar').addEventListener('click',registroGuitarra)
+    document.getElementById('btnEnviar').addEventListener('click',registroGuitarra);
+    recuperarDatos();
+    document.getElementById('btnActualizar').addEventListener('click',actualizaGuitarra);
     // document.getElementById('').addEventListener('click',);
 });
 
@@ -26,6 +28,9 @@ const cambiarColor = ()=>{
     // le cambio la fuente al <h1> de mi html
     fuente.style.fontSize = '50px';
 };
+
+
+
 
 
 
@@ -77,13 +82,119 @@ const registroGuitarra = ()=>{
     if (eDaños.checked) {
         objeto.daños = eDaños.value;
     };
+
     // llamo a la funcion de las promesas para realizar el registro en la base de datos
     registrarGuitarra(objeto).then(()=>{
         alert('se ha registrado')
+        recuperarDatos(); // recupero los datos para ingresarlos a la tabla (la actualizo)
 
         // hago el alert para decirle al usuario que se registró con exito
-    }).catch((e)=>{
+    }).catch((error)=>{
         // en caso de algun error muestro el error en la consola
-        console.log(e)
+        console.log(error)
+    });
+};
+
+
+// actualizar guitarras
+const actualizaGuitarra = () => {
+    let eModelo = document.getElementById('modelo');
+    let eCuerdas = document.getElementById('cuerdas');
+    let eTrastes = document.getElementById('trastes');
+    let ePuente = document.getElementById('puente');
+    let eColor = document.getElementById('color');
+    let eMicrofonos = document.getElementById('microfonos');
+    let eComentarios = document.getElementById('textarea')
+
+    // ahora recupero el valor de los elementos
+    let vModelo = eModelo.value;
+    let vCuerdas = eCuerdas.value;
+    let vTrastes = eTrastes.value;
+    let vPuente = ePuente.value;
+    let vColor = eColor.value;
+    let vMicrofonos = eMicrofonos.value;
+    let vComentarios = eComentarios.value;
+
+    // ahora ingreso todo a un diccionario para llevarlo a la base de datos
+    let objeto = {
+        modelo:vModelo,
+        cuerdas:vCuerdas,
+        trastes:vTrastes,
+        puente:vPuente,
+        color:vColor,
+        microfonos:vMicrofonos,
+        comentarios:vComentarios,
+    };
+
+    let id = document.getElementById('btnActualizar').value;
+    actualizarGuitarra(objeto,id).then(() => {
+        alert('se ha actualizado la guitarra');
+        recuperarDatos();
+    }).catch((error) => {
+        console.log(error)
+    });
+};
+
+// traer los datos de la base datos para mostrarlos
+const recuperarDatos = () => {
+    obtenerGuitarras().then((guitarra) => {
+        let estructura = '';
+        console.log(guitarra);
+
+        // genero la estructura para el html
+        guitarra.forEach((g) => {
+            estructura += '</tr>'
+            estructura += '<td>'+g.modelo+'</td>';
+            estructura += '<td>'+g.cuerdas+'</td>';
+            estructura += '<td>'+g.trastes+'</td>';
+            estructura += '<td>'+g.puente+'</td>';
+            estructura += '<td>'+g.color+'</td>';
+            estructura += '<td>'+g.microfonos+'</td>';
+            estructura += '<td>'+g.falla+'</td>';
+            estructura += '<td>'+g.desteñida+'</td>';
+            estructura += '<td>'+g.daños+'</td>';
+            estructura += '<td>'+g.comentarios+'</td>';
+
+            // genero botones de actualizar y eliminar para los registros en la tabla 
+            estructura += '<td><button id="UPD'+g.id+'">Actualizar</button></td>';
+            estructura += '<td><button id="DEL'+g.id+'">Eliminar</button></td>';
+            estructura += '</tr>';
+        });
+        // muestro la estructura en la consola
+        console.log(estructura);
+        // inserto la estructura en el html
+        document.getElementById('tbGuitarras').innerHTML = estructura
+        guitarra.forEach((g) => {
+            let elemento = document.getElementById('UPD'+g.id);
+            elemento.addEventListener('click', () => {
+
+                document.getElementById('UPDmodelo').value = g.modelo;
+                document.getElementById('UPDcuerdas').value = g.cuerdas;
+                document.getElementById('UPDtrastes').value = g.trastes;
+                document.getElementById('UPDpuente').value = g.puente;
+                document.getElementById('UPDcolor').value = g.color;
+                document.getElementById('UPDmicrofonos').value = g.microfonos;
+                document.getElementById('UPDchecboxFalla').value = g.falla;
+                document.getElementById('UPDchecboxDesteñida').value = g.desteñida;
+                document.getElementById('UPDchecboxDaños').value = g.daños;
+                document.getElementById('UPDcomentarios').value = g.comentarios;
+            });
+            let eliminarElemento = document.getElementById('DEL'+g.id);
+            eliminarElemento.addEventListener('click', () => {
+                eliminar(g.id);
+            });
+        });
+    }).catch((error) => {
+        console.log(error)
+    })
+};
+
+// eliminacion de las guitarras
+const eliminar = (id) => {
+    eliminarGuitarra(id).then(() => {
+        alert('Se eliminó la guitarra correctamente');
+        recuperarDatos(); // Actualizar la tabla después de eliminar
+    }).catch((error) => {
+        console.log(error);
     });
 };
